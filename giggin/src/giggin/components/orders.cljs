c779abd91fd3eedf3101749940c7c0747500e4bd
Yang Sungmin <smyang@nearthlab.com>
2019-05-23 17:19:16 +0900

    09 Remove From Order

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -4,8 +4,8 @@
 ;
 ; (for [[id quant] @state/orders]) -> {:gig-11 2, :gig-07 2}]
 ; (get-in @state/gigs [id :img]
+; (fn [] (swap! state/orders dissoc id))
 ;
-
 
 (defn orders
   []
@@ -20,4 +20,8 @@
         [:div.content
          [:p.title (str (get-in @state/gigs [id :title]) " \u00D7 " quant)]]
         [:div.action
-         [:div.price (* (get-in @state/gigs [id :price]) quant)]]])]]])
+         [:div.price (* (get-in @state/gigs [id :price]) quant)]
+         [:button.btn.btn--link.tooltip
+          {:data-tooltip "Remove"
+           :on-click (fn [] (swap! state/orders dissoc id))}
+          [:i.icon.icon--cross]]]])]]])
