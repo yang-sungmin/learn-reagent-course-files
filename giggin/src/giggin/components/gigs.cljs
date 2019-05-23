2d48d963680ff7c9e67049e95f94089a521cbd97
Yang Sungmin <smyang@nearthlab.com>
2019-05-23 17:08:53 +0900

    07 Add To Order

--- giggin/src/giggin/components/gigs.cljs
+++ giggin/src/giggin/components/gigs.cljs
@@ -1,11 +1,16 @@
 (ns giggin.components.gigs
   (:require [giggin.state :as state]))
-;;
-;; vals
-;; map vs for
-;; :keys in destructuring
-;;
 
+;
+; vals
+; map vs for
+; :keys in destructuring
+; (fn [] (swap! state/orders update id inc)
+; *ns*
+; (shadow.cljs.devtools.api/nrepl-select :app)
+; (ns giggin.state)
+; @giggin.state/orders
+;
 
 (defn gigs
   []
@@ -16,7 +21,9 @@
        [:img.gig__artwork {:src img :alt title}]
        [:div.gig__body
         [:div.gig__title
-         [:div.btn.btn--primary.float--right.tooltip {:data-tooltip "Add to order"}
+         [:div.btn.btn--primary.float--right.tooltip
+          {:data-tooltip "Add to order"
+           :on-click (fn [] (swap! state/orders update id inc))}
           [:i.icon.icon--plus]] title]
         [:p.gig__price price]
         [:p.gig__desc desc]]])]])
